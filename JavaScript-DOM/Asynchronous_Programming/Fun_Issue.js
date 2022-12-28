96101664e3952c3d33bc4f50aeea0e97e7c143e5
Mouataz Billah <mouatazbillah97@gmail.com>
2022-12-28 09:30:53 +0100

    Ways of handling promises

--- JavaScript-DOM/Asynchronous_Programming/Fun_Issue.js
+++ JavaScript-DOM/Asynchronous_Programming/Fun_Issue.js
@@ -10,7 +10,7 @@ function getApproval() {
 
 getApproval();
 result.textContent = approval;
-*/
+
 
 //To fix it we gonna need a call back function
 
@@ -25,3 +25,60 @@ function getApproval(callback) {
     result.textContent = approval;
   });
   result.textContent = approval;
+ 
+
+
+  // Using method then 
+
+  function getApproval() {
+    return new Promise((resolve,reject) => {
+      setTimeout(() => {
+        resolve('aproval!');
+      }, 500);
+    });
+    
+  }
+  
+  getApproval().then(
+    (resolvedAproval)=>{
+      resolve.textContent = resolvedAproval;
+    }
+  );
+
+  result.textContent = approval;
+   */  
+
+
+  //hadling Promises
+
+  let p = new Promise((resolve,reject) => {
+    let a=1+1;
+    if(a==2){
+      resolve('Success');
+    }else{
+      reject('Failed');
+    }
+  });
+
+  p.then((message) =>{
+    console.log('This is in the then' + message );
+  }).catch((message) =>{
+    console.log('This is in the then' + message );
+  });
+
+  //
+
+  function getApproval() {
+    return new Promise((resolve,reject) => {
+      setTimeout(() => {
+        resolve('aproval!');
+      }, 500);
+    });
+  }
+
+  async function setApprovalText(){
+    const approvalPromise = getApproval();
+    result.textContent = await approvalPromise;
+  }
+
+  setApprovalText();
