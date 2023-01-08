e7872816844ab1a213bab7f182df07125740e329
Mouataz Billah <mouatazbillah97@gmail.com>
2023-01-08 16:37:14 +0100

    handling problems using simple exceptions

--- JavaScript-DOM/Asynchronous_Programming/Chanching_Request.js
+++ JavaScript-DOM/Asynchronous_Programming/Chanching_Request.js
@@ -7,48 +7,55 @@ const postContent = document.getElementById('post-content');
 // API URL
 const api = 'https://us-central1-open-classrooms-js-for-the-web.cloudfunctions.net/widgets';
 
-function makeRequest(verb, url, data) {
-  return new Promise((resolve, reject) => {
-    let request = new XMLHttpRequest();
-    request.open(verb, url);
-    request.onreadystatechange = () => {
-      if (request.readyState === 4) {
-        if (request.status === 200 || request.status === 201) {
-          resolve(JSON.parse(request.response));
-        } else {
-          reject(JSON.parse(request.response));
+try{
+  function makeRequest(verb, url, data) {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      request.open(verb, url);
+      request.onreadystatechange = () => {
+        if (request.readyState === 4) {
+          if (request.status === 200 || request.status === 201) {
+            resolve(JSON.parse(request.response));
+          } else {
+            reject(JSON.parse(request.response));
+          }
         }
+      };
+      if (verb === 'POST') {
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify(data));
+      } else {
+        request.send();
       }
-    };
-    if (verb === 'POST') {
-      request.setRequestHeader('Content-Type', 'application/json');
-      request.send(JSON.stringify(data));
-    } else {
-      request.send();
+    });
+  }
+  
+  async function createPost(){
+    const uidPromise =  makeRequest('GET', api + '/generate-uid');
+    const titlePromise =  makeRequest('GET', api + '/generate-title');
+    const loremPromise =  makeRequest('GET', api + '/generate-lorem');
+  
+    const [uidResponse, titleResponse, loremResonse] = await Promise.all([uidPromise, titlePromise, loremPromise]);
+    
+    const postPromise = makeRequest('POST', api + '/create-post-width-uid',{
+      uid: uidResponse.uid,
+      title: titleResponse.title,
+      content: loremResonse.lorem
+    });
+    try{
+      const postResponse = await postPromise; 
+  
+      postTitle.textContent = postResponse.post.title;
+      postId.textContent = postResponse.post.id;
+      postContent.textContent = postResponse.post.content;
+    }catch (error){
+      postTitle.textContent = error.error;
     }
-  });
+    
+  }
+}catch (error){
+  postTitle.textContent = error.error;
 }
-
-async function createPost(){
-  const uidPromise =  makeRequest('GET', api + '/generate-uid');
-  const titlePromise =  makeRequest('GET', api + '/generate-title');
-  const loremPromise =  makeRequest('GET', api + '/generate-lorem');
-
-  const [uidResponse, titleResponse, loremResonse] = await Promise.all([uidPromise, titlePromise, loremPromise]);
-
-  const postPromise = makeRequest('POST', api + '/create-post-width-uid',{
-    uid: uidResponse.uid,
-    title: titleResponse.title,
-    content: loremResonse.lorem
-  });
-
-  const postResponse = await postPromise; 
-
-  postTitle.textContent = postResponse.post.title;
-  postId.textContent = postResponse.post.id;
-  postContent.textContent = postResponse.post.content;
-}
-
 generateButton.addEventListener('click', () =>{
   createPost();
 });
